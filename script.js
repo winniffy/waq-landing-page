a1d29aadda66bbe3625d771026b2cfb4b3528188
winner umeh <winniffy@gmail.com>
2023-05-14 19:58:47 +0100

    implementing scrollintoview

--- script.js
+++ script.js
@@ -1,18 +1,78 @@
 'use strict'
 
 
-
-
-// tabbed component - contribute section
 const tabs = document.querySelectorAll('.contribute_tab');
 const tabsContainer = document.querySelector('.contribute_tab-container');
 const tabsContent = document.querySelectorAll('.contribute_content');
 
+const header = document.querySelector('.header');
+const nav = document.querySelector('.nav');
+
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav_list_group');
+const overlay = document.querySelector('.overlay');
+const navItems = document.querySelectorAll('.nav_item');
+
+// hamburger
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    overlay.classList.toggle("active");
+});
+
+// sticky nav
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function(entries) {
+    const [entry] = entries;
+
+    if(!entry.isIntersecting) nav.classList.add('sticky');
+    else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver
+( stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
+});
+
+// headerObserver.observe(header);
+
+// scroll to contribute
+const contributeScroll = document.querySelector('.btn-contribute');
+const section3 = document.querySelector('#section-3')
+
+contributeScroll.addEventListener('click', function(e) {
+    // e.preventDefault();
+
+    section3
+    .scrollIntoView({behavior: "smooth"});
+})
+
+
+
+// nav links scroll to section
+document.querySelector('.nav_list_group').addEventListener('click', function(e) {
+    e.preventDefault();
+    // console.log(e.target);
+
+    if(e.target.classList.contains('nav_link')) {
+        const id = e.target.getAttribute('href');
+        console.log(id)
+        document.querySelector(id)
+        .scrollIntoView({behavior: "smooth"});
+    }
+});
+
+
+// tabbed component - contribute section
 tabsContainer.addEventListener('click', function(e) {
     const clicked = e.target.closest('.contribute_tab');
     // console.log(clicked);
 
-    //gaurd clause
+    //guard clause
     if (!clicked) return;
 
     tabs.forEach(tab => tab.classList.remove('contribute_tab-active'));
